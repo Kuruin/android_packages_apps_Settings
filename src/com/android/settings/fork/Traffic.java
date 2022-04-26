9c96f6f901cdf72083289d1ae62b78b15a786df6
Kuruin <manavjit9@gmail.com>
2022-04-26 07:01:47 +0000

    Revert "Settings: Add Network Traffic Layout [2/2]"
    This reverts commit f847f025f38c803856cb604b8c7a44ada989960d.

--- src/com/android/settings/fork/Traffic.java
+++ src/com/android/settings/fork/Traffic.java
@@ -62,7 +62,6 @@ public class Traffic extends SettingsPreferenceFragment implements OnPreferenceC
     private CustomSeekBarPreference mNetTrafficSize;
     private CustomSeekBarPreference mThreshold;
     private SystemSettingSwitchPreference mShowArrows;
-    private ListPreference mNetTrafficLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -72,13 +71,6 @@ public class Traffic extends SettingsPreferenceFragment implements OnPreferenceC
 
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
-
-        mNetTrafficLayout = (ListPreference) findPreference("network_traffic_layout");
-        int netlayout = Settings.System.getIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_LAYOUT, 0, UserHandle.USER_CURRENT);
-        mNetTrafficLayout.setValue(String.valueOf(netlayout));
-        mNetTrafficLayout.setSummary(mNetTrafficLayout.getEntry());
-        mNetTrafficLayout.setOnPreferenceChangeListener(this);
 
         int NetTrafficSize = Settings.System.getInt(resolver,
                 Settings.System.NETWORK_TRAFFIC_FONT_SIZE, 36);
@@ -112,7 +104,7 @@ public class Traffic extends SettingsPreferenceFragment implements OnPreferenceC
             updateTrafficLocation(location+1);
         } else {
             mNetTrafficLocation.setValue("0");
-            updateTrafficLocation(0);
+            updateTrafficLocation(0); 
         }
         mNetTrafficLocation.setSummary(mNetTrafficLocation.getEntry());
     }
@@ -160,14 +152,6 @@ public class Traffic extends SettingsPreferenceFragment implements OnPreferenceC
             int width = ((Integer)objValue).intValue();
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NETWORK_TRAFFIC_FONT_SIZE, width);
-            return true;
-        } else if (preference == mNetTrafficLayout) {
-            int val = Integer.valueOf((String) objValue);
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.NETWORK_TRAFFIC_LAYOUT, val,
-                    UserHandle.USER_CURRENT);
-            int index = mNetTrafficLayout.findIndexOfValue((String) objValue);
-            mNetTrafficLayout.setSummary(mNetTrafficLayout.getEntries()[index]);
             return true;
         }
         return false;
